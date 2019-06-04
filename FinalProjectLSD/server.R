143340cc8d0121a47c1e4b14d3832f95acb4dfa5
Darian Baruso <Daarruso@gmail.com>
2019-06-04 02:55:54 -0700

    App logic defined for plots 1 through 3. Y axes to be improved. References from ui.R adjusted to match server.R

--- FinalProjectLSD/server.R
+++ FinalProjectLSD/server.R
@@ -1,31 +1,116 @@
-#
-# This is the server logic of a Shiny web application. You can run the
-# application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
-
 library(shiny)
+library(dplyr)
+library(stringr)
+library(ggplot2)
+
+movieData <- read.csv("./data/tmdb_5000_movies.csv")
 
 # Define server logic required to draw a histogram
 shinyServer(function(input, output) {
+    
+    # Reactive expressions created in order to paste statistics to plots
+    plot1DataText <- reactive({
+        movieData %>%
+            filter(str_detect(genres, input$plot1Select)) %>%
+            select(c(genres, revenue))
+    })
+    
+    plot2DataText <- reactive({
+        movieData %>%
+            filter(str_detect(genres, input$plot2Select)) %>%
+            select(c(genres, vote_average))
+    })
+    
+    plot3DataText <- reactive({
+        movieData %>%
+            filter(str_detect(genres, input$plot3Select)) %>%
+            select(c(genres, budget))
+    })
+    
+    # might not be necessary since there isn't something like genre to generalize?
+    plot4DataText <- reactive({
+        movieData %>%
+            filter(str_detect(genres, input$plot4Range)) %>%
+            select(c(budget, revenue))
+    })
 
-    output$distPlot <- renderPlot({
-
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x))
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white')
+    output$plot1 <- renderPlot({
+        plot1Data <- movieData %>% 
+            filter(str_detect(genres, input$plot1Select)) %>% 
+            select(c(genres, revenue))
+        plot1Min <- min(plot1Data$revenue)
+        plot1Max <- max(plot1Data$revenue)
+        plot1Mean <- mean(plot1Data$revenue)
+        plot1Title <- c(paste0(input$plot1Select, ", Min"), 
+                        paste0(input$plot1Select, ", Max"), 
+                        paste0(input$plot1Select, ", Mean"))
+        plot1Values <- c(plot1Min, plot1Max, plot1Mean)
+        plot1df <- data.frame(plot1Title, plot1Values)
+        plot1df %>%
+            ggplot(aes(x = plot1Title, y = plot1Values)) +
+            geom_boxplot(fill = "cadetblue1", alpha = 0.5) +
+            labs(x = "Genre", y = "Revenue")
 
     })
     
-    output$Revenue <- renderPlot({
-      
+    output$p1Text <- renderText({
+        
     })
     
+    output$plot2 <- renderPlot({
+        plot2Data <- movieData %>% 
+            filter(str_detect(genres, input$plot2Select)) %>% 
+            select(c(genres, vote_average))
+        plot2Min <- min(plot2Data$vote_average)
+        plot2Max <- max(plot2Data$vote_average)
+        plot2Mean <- mean(plot2Data$vote_average)
+        plot2Title <- c(paste0(input$plot2Select, ", Min"), 
+                        paste0(input$plot2Select, ", Max"), 
+                        paste0(input$plot2Select, ", Mean"))
+        plot2Values <- c(plot2Min, plot2Max, plot2Mean)
+        plot2df <- data.frame(plot2Title, plot2Values)
+        plot2df %>%
+            ggplot(aes(x = plot2Title, y = plot2Values)) +
+            geom_boxplot(fill = "coral2", alpha = 0.5) +
+            labs(x = "Genre", y = "Average Rating received")
+    })
+    
+    output$p2Text <- renderText({
+        
+    })
+    
+    output$plot3 <- renderPlot({
+        plot3Data <- movieData %>% 
+            filter(str_detect(genres, input$plot3Select)) %>% 
+            select(c(genres, budget))
+        plot3Min <- min(plot3Data$budget)
+        plot3Max <- max(plot3Data$budget)
+        plot3Mean <- mean(plot3Data$budget)
+        plot3Title <- c(paste0(input$plot3Select, ", Min"), 
+                        paste0(input$plot3Select, ", Max"), 
+                        paste0(input$plot3Select, ", Mean"))
+        plot3Values <- c(plot3Min, plot3Max, plot3Mean)
+        plot3df <- data.frame(plot3Title, plot3Values)
+        plot3df %>%
+            ggplot(aes(x = plot3Title, y = plot3Values)) +
+            geom_boxplot(fill = "aquamarine1", alpha = 0.5) +
+            labs(x = "Genre", y = "Budget Received")
+    })
+    
+    output$p3Text <- renderText({
+        
+    })
+    
+    output$plot4 <- renderPlot({
+        
+    })
+    
+    output$p4Text <- renderText({
+        
+    })
+    
+    output$about <- renderText({
+        
+    })
 
 })
--- FinalProjectLSD/ui.R
+++ FinalProjectLSD/ui.R
@@ -1,86 +1,83 @@
-#
-# This is the user-interface definition of a Shiny web application. You can
-# run the application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
-
 library(shiny)
 
-CHOICE <- c("Horror","Comedy","Action","Romance","Thriller","Sci-fi","Drama","Documentary","All")
-# Define UI for application that draws a histogram
+CHOICE <- c("Action" = "Action", "Adventure" = "Adventure",
+           "Comedy" = "Comedy", "Documentary" = "Documentary",
+           "Drama" = "Drama", "Horror" = "Horror", "Romance" = "Romance",
+           "Sci-Fi" = "Science Fiction", "Thriller" = "Thriller")
 shinyUI(fluidPage(
-  
-  # Application title
-  titlePanel("Final Project"),
-  
-  # Sidebar with a slider input for number of bins
-  navbarPage("Home",
-             tabPanel("Summary"
-                      
-             ),
-             tabPanel("Plot1",
-                      sidebarLayout(
-                        sidebarPanel(
-                          selectInput("Plot1select",
-                                      label = h3("Selectbox"),
-                                      choices = CHOICE)
-                        ),
-                        mainPanel(
-                          plotOutput("plot1")
+    
+    # Application title
+    titlePanel("Final Project"),
+    
+    # Sidebar with a slider input for number of bins
+    navbarPage("Home",
+               tabPanel("Summary"
+                        
+               ),
+               tabPanel("Revenue",
+                        sidebarLayout(
+                            sidebarPanel(
+                                selectInput("plot1Select",
+                                            label = h3("Selectbox"),
+                                            choices = CHOICE)
+                            ),
+                            mainPanel(
+                                plotOutput("plot1")#,
+                                # textOutput("p1Text")
+                            )
                         )
-                      )
-             ),
-             tabPanel("Plot2",
-                      sidebarLayout(
-                        sidebarPanel(
-                          selectInput("Plot2select",
-                                      label = h3("Selectbox"),
-                                      choices = CHOICE)
-                        ),
-                        mainPanel(
-                          plotOutput("plot2")
+               ),
+               tabPanel("Ratings",
+                        sidebarLayout(
+                            sidebarPanel(
+                                selectInput("plot2Select",
+                                            label = h3("Selectbox"),
+                                            choices = CHOICE)
+                            ),
+                            mainPanel(
+                                plotOutput("plot2")#,
+                                # textOutput("p2Text")
+                            )
                         )
-                      )
-             ),
-             tabPanel("Plot3",
-                      sidebarLayout(
-                        sidebarPanel(
-                          selectInput("Plot3select",
-                                      label=h3("Selectbox"),
-                                      choices = CHOICE)
-                        ),
-                        mainPanel(
-                          plotOutput("plot3")
+               ),
+               tabPanel("Budget",
+                        sidebarLayout(
+                            sidebarPanel(
+                                selectInput("plot3Select",
+                                            label=h3("Selectbox"),
+                                            choices = CHOICE)
+                            ),
+                            mainPanel(
+                                plotOutput("plot3")#,
+                                # textOutput("p3Text")
+                            )
                         )
-                      )
-             ),
-             tabPanel("Plot4",
-                      sidebarLayout(
-                        sidebarPanel(
-                          sliderInput("range",
-                                      label = h3("Slider Range in Millions"),
-                                      min = 0,
-                                      max = 400,
-                                      value = c(0,400))
-                        ),
-                        mainPanel(
-                          plotOutput("plot4")
+               ),
+               tabPanel("Revenue and Budget",
+                        sidebarLayout(
+                            sidebarPanel(
+                                sliderInput("range",
+                                            label = h3("Slider Range in Millions"),
+                                            min = 0,
+                                            max = 400,
+                                            value = c(0,400))
+                            ),
+                            mainPanel(
+                                plotOutput("plot4")#,
+                                # textOutput("p4Text")
+                            )
                         )
-                      )
-               
-             ),
-             tabPanel("About Us",
-                      sidebarLayout(
-                        sidebarPanel(
-                          
-                        ),
-                        mainPanel(
-                          textOutput("about")
+                        
+               ),
+               tabPanel("About Us",
+                        sidebarLayout(
+                            sidebarPanel(
+                                
+                            ),
+                            mainPanel(
+                                textOutput("about")
+                            )
                         )
-                      )
-             )
-  )
+               )
+    )
 ))
