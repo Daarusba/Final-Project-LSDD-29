577b25f1834785c03d1b4887d1cb06e072048721
Shinichiro Aoyagi <saoyagi16@gmail.com>
2019-06-03 22:38:14 -0700

    skeleton

--- FinalProjectLSD/ui.R
+++ FinalProjectLSD/ui.R
@@ -9,25 +9,78 @@
 
 library(shiny)
 
+CHOICE <- c("Horror","Comedy","Action","Romance","Thriller","Sci-fi","Drama","Documentary","All")
 # Define UI for application that draws a histogram
 shinyUI(fluidPage(
-
-    # Application title
-    titlePanel("Old Faithful Geyser Data"),
-
-    # Sidebar with a slider input for number of bins
-    sidebarLayout(
-        sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
-        ),
-
-        # Show a plot of the generated distribution
-        mainPanel(
-            plotOutput("distPlot")
-        )
-    )
+  
+  # Application title
+  titlePanel("Final Project"),
+  
+  # Sidebar with a slider input for number of bins
+  navbarPage("Home",
+             tabPanel("Summary"
+                      
+             ),
+             tabPanel("Plot1",
+                      sidebarLayout(
+                        sidebarPanel(
+                          selectInput("Plot1select",
+                                      label = h3("Selectbox"),
+                                      choices = CHOICE)
+                        ),
+                        mainPanel(
+                          plotOutput("plot1")
+                        )
+                      )
+             ),
+             tabPanel("Plot2",
+                      sidebarLayout(
+                        sidebarPanel(
+                          selectInput("Plot2select",
+                                      label = h3("Selectbox"),
+                                      choices = CHOICE)
+                        ),
+                        mainPanel(
+                          plotOutput("plot2")
+                        )
+                      )
+             ),
+             tabPanel("Plot3",
+                      sidebarLayout(
+                        sidebarPanel(
+                          selectInput("Plot3select",
+                                      label=h3("Selectbox"),
+                                      choices = CHOICE)
+                        ),
+                        mainPanel(
+                          plotOutput("plot3")
+                        )
+                      )
+             ),
+             tabPanel("Plot4",
+                      sidebarLayout(
+                        sidebarPanel(
+                          sliderInput("range",
+                                      label = h3("Slider Range in Millions"),
+                                      min = 0,
+                                      max = 400,
+                                      value = c(0,400))
+                        ),
+                        mainPanel(
+                          plotOutput("plot4")
+                        )
+                      )
+               
+             ),
+             tabPanel("About Us",
+                      sidebarLayout(
+                        sidebarPanel(
+                          
+                        ),
+                        mainPanel(
+                          textOutput("about")
+                        )
+                      )
+             )
+  )
 ))
--- README.md
+++
@@ -1,2 +0,0 @@
-# Final-Project-LSDD-29
-The final project of Group LSDD during INFO 201's Spring quarter of 2019
