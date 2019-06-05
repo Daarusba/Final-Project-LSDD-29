7ae81839590e0288183fff9549bfdaeed17071c4
Lelo Gemtessa <lelogemtessa@D-10-19-85-128.dhcp4.washington.edu>
2019-06-04 21:55:28 -0700

    range widget fixed

--- FinalProjectLSD/server.R
+++ FinalProjectLSD/server.R
@@ -84,10 +84,10 @@ shinyServer(function(input, output) {
     })
     
     output$plot4 <- renderPlot({
-        plot4Data <- movieData %>% 
-            #filter(str_detect(budget,input$range)) %>% 
-            select(c(budget, revenue))
-        ggplot(movieData, aes(x= signif((plot4Data$budget/ 1000000), digits = 3), y= signif((plot4Data$revenue/ 1000000), digits = 3))) +
+        plot4data <- movieData %>% 
+            mutate(budget = signif(budget / 1000000, digits = 3), revenue = signif(revenue / 1000000, digits = 3)) %>% 
+            filter(budget >= input$range[1], budget <= input$range[2])
+        ggplot(plot4data, aes(x= budget, y= revenue)) +
             geom_point() +
             geom_smooth(method=lm) +
             labs(title = "Budget and Revenue Correlation", x = "Budget Received (Millions of USD)", y = "Revenue (Millions of USD)")
